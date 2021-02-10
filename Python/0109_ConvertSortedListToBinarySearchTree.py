7cc2ab6a78606895c7d1262c1271c8896a80adc2
wwilliam98 <wwilliam1908@gmail.com>
2021-02-09 19:47:25 -0600

    Find the middle, convert it to tree

---
+++ Python/0109_ConvertSortedListToBinarySearchTree.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        if not head:
+            return None
+            
+        mid = self.findMid(head)
+        
+        node = TreeNode(mid.val)
+        
+        if head == mid:
+            return node
+        
+        node.left = self.sortedListToBST(head)
+        node.right = self.sortedListToBST(mid.next)
+        return node
+        
+    
+    def findMid(self, node):
+        prev = None
+        slow = fast = node
+        
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        if prev:
+            prev.next = None
+        
+        return slow
