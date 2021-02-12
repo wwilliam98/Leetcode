7c1678cb12909114293b11d071a571a3b61f77cc
wwilliam98 <wwilliam1908@gmail.com>
2021-02-12 01:52:18 -0600

    DFS

---
+++ Python/0106_ConstructBinaryTreeFromInorderAndPostorderTraversal.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        print(inorder, postorder)
+        if not inorder or not postorder:
+            return None
+            
+        idx = inorder.index(postorder.pop())
+        root = TreeNode(inorder[idx])
+        root.right = self.buildTree(inorder[idx+1:], postorder)
+        root.left = self.buildTree(inorder[:idx], postorder)
+        
+        return root
