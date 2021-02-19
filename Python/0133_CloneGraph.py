654a31582b92a8a70686bf90a6365da9ee44403c
wwilliam98 <wwilliam1908@gmail.com>
2021-02-18 20:26:57 -0600

    DFS/BFS, Graph, Recusion

--- Python/0133_CloneGraph.py
+++ Python/0133_CloneGraph.py
@@ -5,7 +5,7 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
+#DSF (hardcore)
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         self.visited = {}
@@ -25,3 +25,31 @@ class Solution:
             clone.neighbors.append(self.helper(i))
         
         return clone
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+#BFS
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+        
+        visited = {}
+        q = [node]
+        visited[node] = Node(node.val, []) #set map (realnode: clone)
+        while q:
+            n = q.pop(0)
+            
+            for neighbor in n.neighbors:
+                if neighbor not in visited: #When its not in map, dont put it in the map nor q
+                    visited[neighbor] = Node(neighbor.val, [])
+                    q.append(neighbor)
+                    
+                visited[n].neighbors.append(visited[neighbor]) #append cloned neighbor to cloned node neighbor
+        
+        return visited[node] #Ta-da, just return the node
