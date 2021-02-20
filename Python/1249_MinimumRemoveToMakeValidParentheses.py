6eccbc168bc632e8b892889ec5d0eb408aa0e7e3
wwilliam98 <wwilliam1908@gmail.com>
2021-02-19 18:17:26 -0600

    String to list, and Two-Pass solution

--- Python/1249_MinimumRemoveToMakeValidParentheses.py
+++ Python/1249_MinimumRemoveToMakeValidParentheses.py
@@ -1,3 +1,25 @@
+#store string as list
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        open_par = []
+        s = list(s)
+        
+        for idc, char in enumerate(s):
+            if char == '(':
+                open_par.append(idc)
+            elif char == ')':
+                if open_par:
+                    open_par.pop()
+                else:
+                    s[idc] = ""
+                    
+            
+        while open_par:
+            s[open_par.pop()] = ""
+            
+        return "".join(s)
+
+#Two Pass solution
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         res = ""
