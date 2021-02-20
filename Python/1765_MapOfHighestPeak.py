407e3e24421768651224e42f57afdb22b0259411
wwilliam98 <wwilliam1908@gmail.com>
2021-02-20 14:26:32 -0600

    BFS

--- Python/1765_MapOfHighestPeak.py
+++ Python/1765_MapOfHighestPeak.py
@@ -1,4 +1,35 @@
-q = []
+#Change height in Place
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        q = []
+        nr = len(isWater)
+        nc = len(isWater[0])
+        visited = set()
+        
+        for i in range(nr):
+            for j in range(nc):
+                if isWater[i][j] == 1:
+                    isWater[i][j] = 0
+                    q.append((i,j))
+                    visited.add((i,j))
+                    
+        directions = [(-1,0), (0,-1), (1,0), (0,1)]
+        while q:
+            row, col = q.pop(0)
+            
+            for dr, dc in directions:
+                neighR, neighC = row + dr, col + dc
+                if 0 <= neighR < nr and 0 <= neighC < nc and (neighR, neighC) not in visited:
+                    isWater[neighR][neighC] = isWater[row][col] + 1
+                    visited.add((neighR,neighC))
+                    q.append((neighR, neighC))
+                    
+        return isWater
+
+#Store the initial island
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        q = []
         first = []
         nr = len(isWater)
         nc = len(isWater[0])
