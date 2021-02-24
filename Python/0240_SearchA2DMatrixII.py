1508790433a2bd039496bad733c88c1a1e7498fe
wwilliam98 <wwilliam1908@gmail.com>
2021-02-24 00:37:01 -0600

    search space reduction, O(m+n)

--- Python/0240_SearchA2DMatrixII.py
+++ Python/0240_SearchA2DMatrixII.py
@@ -1,3 +1,25 @@
+#O(m+n) approach, search space reduction
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        row = m-1
+        col = 0
+        
+        while 0 <= row and col < n:
+            if matrix[row][col] < target:
+                col += 1
+            elif matrix[row][col] > target:
+                row -= 1
+            else:
+                return True
+        return False
+
+#O(m*n) approach
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         col = -1
