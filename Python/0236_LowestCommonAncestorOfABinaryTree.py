f881c7ad4cb5486a8ab0905ef758c182d93e7bc6
wwilliam98 <wwilliam1908@gmail.com>
2021-02-26 17:34:38 -0600

    DFS/Iteratively

--- Python/0236_LowestCommonAncestorOfABinaryTree.py
+++ Python/0236_LowestCommonAncestorOfABinaryTree.py
@@ -1,3 +1,4 @@
+## DFS, mark a flag if its, p or q. If True is more than 2, set the answer.
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -15,12 +16,44 @@ class Solution:
         if not node:
             return False
         
+        flag = node == p or node == q
         left = self.dfs(node.left, p, q)
         right = self.dfs(node.right, p, q)
-        
-        flag = node == p or node == q
         
         if left + right + flag >= 2:
             self.ret = node
         
         return flag or left or right
+
+## Queue, put the parents in dictionary, put all ancestors of p in set, find q that is in the ancestors
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        queue = [root]
+        parents = {root: None}
+        
+        while p not in parents or q not in parents:
+            node = queue.pop(0)
+            if node.left:
+                parents[node.left] = node
+                queue.append(node.left)
+                
+            if node.right:
+                parents[node.right] = node
+                queue.append(node.right)
+                
+        ancestors = set()
+        while p:
+            ancestors.add(p)
+            p = parents[p]
+        
+        while q not in ancestors:
+            q = parents[q]
+        
+        return q
