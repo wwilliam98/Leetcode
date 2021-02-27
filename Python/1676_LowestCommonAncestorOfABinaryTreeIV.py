3cfff20cd2ccd626f9a481a87a73a1f59c1a5423
wwilliam98 <wwilliam1908@gmail.com>
2021-02-27 12:31:55 -0600

    find the earliest node, then go right, if found node on both, return that node, else, return the found node

---
+++ Python/1676_LowestCommonAncestorOfABinaryTreeIV.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
+        if not root or root in nodes:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, nodes)
+        right = self.lowestCommonAncestor(root.right,nodes)
+        
+        if left and right:
+            return root
+        
+        else:
+            if left:
+                return left
+            else:
+                return right
