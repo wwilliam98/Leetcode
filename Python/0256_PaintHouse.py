46aaa8fbcf38c4ccf42723b69b32dba6e1c116df
wwilliam98 <wwilliam1908@gmail.com>
2021-03-03 23:31:29 -0600

    DP, calculate every possibility for each row

--- Python/0256_PaintHouse.py
+++ Python/0256_PaintHouse.py
@@ -1,10 +1,10 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        for endrow in range(len(costs)-2, -1, -1):
-            costs[endrow][0] += min(costs[endrow+1][1], costs[endrow+1][2])
-            costs[endrow][1] += min(costs[endrow+1][0], costs[endrow+1][2])
-            costs[endrow][2] += min(costs[endrow+1][0], costs[endrow+1][1])
-        
+        for endrow in range(1, len(costs)):
+            costs[endrow][0] += min(costs[endrow-1][1], costs[endrow-1][2])
+            costs[endrow][1] += min(costs[endrow-1][0], costs[endrow-1][2])
+            costs[endrow][2] += min(costs[endrow-1][0], costs[endrow-1][1])
+        print(costs)
         if not costs:
             return 0
-        return min(costs[0])
+        return min(costs[-1])
