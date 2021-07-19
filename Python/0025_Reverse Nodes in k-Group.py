3f5b2d481661e35965c1d378d290dffb05956191
wwilliam98 <wwilliam1908@gmail.com>
2021-07-18 22:51:03 -0500

    iterative, recursion

--- Python/0025_Reverse Nodes in k-Group.py
+++ Python/0025_Reverse Nodes in k-Group.py
@@ -1,3 +1,44 @@
+#Iteratively
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        prev_tail, curr = None, head
+        ret = None
+
+        while curr:
+            counter = 0
+            head = curr
+
+            while counter < k and curr:
+                curr = curr.next
+                counter += 1
+
+            if counter == k:
+                reversedHead = self.reverseList(head, k)
+
+                if not ret:
+                    ret = reversedHead
+
+                if prev_tail:
+                    prev_tail.next = reversedHead
+
+                prev_tail = head
+                head = curr
+
+        if prev_tail:
+            prev_tail.next = head
+
+        return ret if ret else head
+
+    def reverseList(self, head, k):
+        prev, curr = None, head
+
+        while k:
+            curr.next, prev, curr = prev, curr, curr.next
+            k-=1
+
+        return prev
+
+#Recursion time = O(n), space = O(n)
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
         counter = 0
