222968d3fe47ffed058bf8fc456ecd0031c170d3
wwilliam98 <wwilliam1908@gmail.com>
2022-01-27 04:59:17 +0700

    update

--- Python/0121_BestTimeToBuyAndSellStock.py
+++ Python/0121_BestTimeToBuyAndSellStock.py
@@ -12,3 +12,15 @@ class Solution:
                 p1 = p2
             p2 += 1
         return maxProfit
+
+#More Readable solution
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float("inf")
+        max_profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
+        return max_profit
