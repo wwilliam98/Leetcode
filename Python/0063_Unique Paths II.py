b2872996f534cc3cba4075e36dc6c63c9f1be0b2
wwilliam98 <wwilliam1908@gmail.com>
2022-05-20 16:41:45 +0800

    if there is obstacle in the first cell, the robot wont be able to make anymore move, so the rest of the first row/column will be 0

--- Python/0063_Unique Paths II.py
+++ Python/0063_Unique Paths II.py
@@ -1,23 +1,27 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0] * n for _ in range(m)]
-        if obstacleGrid[0][0] == 1:
+        r = len(obstacleGrid)
+        c = len(obstacleGrid[0])
+        dp = [[0 for _ in range(c)] for _ in range(r)]
+        
+        if obstacleGrid[0][0]:
             return 0
-
-        #Setup initial case, when there is rock on first col/row blocking the path, the path after the rock will all be 0
+        
         dp[0][0] = 1
-        for row in range(1, m):
-            if obstacleGrid[row][0] == 0 and dp[row-1][0] == 1:
-                dp[row][0] = 1
-        for col in range(1, n):
-            if obstacleGrid[0][col] == 0 and dp[0][col-1] == 1:
-                dp[0][col] = 1
-
-        #DP like in Unique Path
-        for row in range(1, m):
-            for col in range(1, n):
-                if obstacleGrid[row][col] == 0:
-                    dp[row][col] = dp[row-1][col] + dp[row][col-1]
-
-        return dp[m-1][n-1]
+        for i in range(1, r):
+            dp[i][0] = int(obstacleGrid[i][0] == 0 and obstacleGrid[i-1][0] == 0)
+            if obstacleGrid[i][0] == 1:
+                break
+            
+        for j in range(1, c):
+            dp[0][j] = int(obstacleGrid[0][j] == 0 and obstacleGrid[0][j-1] == 0)
+            if obstacleGrid[0][j] == 1:
+                break
+        
+        for i in range(1, r):
+            for j in range(1, c):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[r-1][c-1]
