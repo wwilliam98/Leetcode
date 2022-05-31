4c951ac403cfd54c68e64a22a387d199ed07ce5a
wwilliam98 <wwilliam1908@gmail.com>
2022-05-31 20:37:53 +0800

    add decimal version

--- Python/1461_CheckIfAStringContainsAllBinaryCodesOfSizeK.py
+++ Python/1461_CheckIfAStringContainsAllBinaryCodesOfSizeK.py
@@ -12,3 +12,15 @@ class Solution:
                 if need == 0:
                     return True
         return False
+    
+#Decimal Version
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        table = [0] * (2**k) #create hash table
+        for i in range(k-1, len(s)):
+            table[int(s[i-k+1:i+1], 2)] = 1 #save to hash table
+
+        for i in table: #check if all hash table is 1
+            if i == 0:
+                return False
+        return True
